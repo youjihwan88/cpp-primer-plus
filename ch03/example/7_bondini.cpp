71797c239acbe366c1bfbf49df1335eb2bb8d85d
JiHwan You <youjihwan88@gmail.com>
2025-08-03 20:33:15 +0900

    feat: add chapter 3 examples

---
+++ ch03/example/7_bondini.cpp
@@ -0,0 +1,19 @@
+// bondini.cpp -- using escape sequences
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "\aOperation \"HyperHype\" is now activated!\n";
+    cout << "Enter you agent code:________\b\b\b\b\b\b\b\b";
+
+    long code;
+    cin >> code;
+
+    cout << "\aYou entered " << code << "...\n";
+    cout << "\aCode verified! Processed with Plan Z3!\n";
+
+    return 0;
+}
