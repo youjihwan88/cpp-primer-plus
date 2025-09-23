96e0c3fba9f68b6af86217e702cbc3ba304faea5
JiHwan You <youjihwan88@gmail.com>
2025-09-23 22:12:33 +0900

    feat: add chapter 11 examples

---
+++ ch11/examples/21_stone1.cpp
@@ -0,0 +1,16 @@
+// stone1.cpp -- user-defined conversion functions
+#include <iostream>
+#include "19_stonewt1.h"
+
+int main()
+{
+    using std::cout;
+    Stonewt poppins(9, 2.8);
+    double p_wt = poppins;
+    cout << "Convert to double => ";
+    cout << "Poppins: " << p_wt << " pounds.\n";
+    cout << "Convert to int => ";
+    cout << "Poppins: " << int(poppins) << " pounds.\n";
+
+    return 0;
+}
---
+++ ch11/examples/3_usetime0.cpp
@@ -0,0 +1,32 @@
+// usetim0.cpp -- using the first draft of the Time class
+#include <iostream>
+#include "1_mytime0.h"
+
+int main()
+{
+    using std::cout, std::endl;
+
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    cout << "planning time = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "coding time = ";
+    coding.Show();
+    cout << endl;
+
+    cout << "fixing time = ";
+    fixing.Show();
+    cout << endl;
+
+    total = coding.Sum(fixing);
+    cout << "coding.Sum(fixing) = ";
+    total.Show();
+    cout << endl;
+
+    return 0;
+}
